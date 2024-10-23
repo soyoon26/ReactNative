1a5cb3c4340def89ae908d92e47fa55d41961e15
soyoon26 <barcardi26@gmail.com>
2024-10-23 20:18:39 +0900

    카카오톡 친구목록 : styled-components 적용

--- FriendsList/src/Profile.js
+++ FriendsList/src/Profile.js
@@ -1,37 +1,43 @@
 import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import styled from "styled-components/native";
 import Margin from "./Margin";
 
-export default (uri, name, introduction, isMe) => {
+export default ({ uri, name, introduction, isMe }) => {
   const size = isMe ? 50 : 40;
+  const Container = styled.View`
+    flex-direction: row;
+  `;
+  const ProfileImage = styled.Image`
+    width: ${(props) => props.size}px;
+    height: ${(props) => props.size}px;
+    border-radius: ${(props) => props.size * 0.4}px;
+  `;
+  const TextContainer = styled.View`
+    justify-content: center;
+    margin-left: 10px;
+  `;
+  const NameText = styled.Text`
+    font-weight: ${(props) => (props.isMe ? "bold" : "normal")};
+    font-size: ${(props) => (props.isMe ? 16 : 15)}px;
+  `;
+  const IntroductionText = styled.Text`
+    font-size: ${(props) => (props.isMe ? 12 : 11)}px;
+    color: grey;
+  `;
   return (
-    <View style={{ flexDirection: "row" }}>
-      <Image
-        source={{ uri: uri }}
-        style={{
-          width: 50,
-          height: size,
-          borderRadius: size * 0.4,
-        }}
-      />
-      <View style={{ justifyContent: "center", marginLeft: 10 }}>
-        <Text
-          style={{
-            fontWeight: isMe ? "bold" : undefined,
-            fontSize: isMe ? 16 : 15,
-          }}
-        >
-          {name}
-        </Text>
+    <Container>
+      <ProfileImage source={{ uri: uri }} size={size} />
+      <TextContainer>
+        <NameText isMe={isMe}>{name}</NameText>
         {!!introduction && (
-          <View style={{ height: isMe ? 6 : 2 }}>
-            <Text style={{ fontSize: isMe ? 12 : 11, color: "grey" }}>
-              {introduction}
-            </Text>
+          <View>
+            <Margin height={isMe ? 6 : 2} />
+            <IntroductionText isMe={isMe}>{introduction}</IntroductionText>
           </View>
         )}
-      </View>
-    </View>
+      </TextContainer>
+    </Container>
   );
 };
 
